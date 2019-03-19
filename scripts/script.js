a1aae6ae9e67b4e89c4daf33733645d9946b3728
Alanna <oconnor.alannae@gmail.com>
2019-03-18 23:59:57 -0400

    media queries

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,34 @@ $(function () {
 
   ScrollReveal().reveal('.portfolioItemContainer', {delay: 200})
 
-  $('a').smoothScroll({
-    offset: 0,      
-    speed: 1000
-  });
+  $('.aboutLink').on('click', function() {
+    console.log("hi")
+    $.smoothScroll({
+      scrollTarget: '#about',
+      speed: 1000
+    })
+  })
+
+  $('.portfolioLink').on('click', function () {
+    $.smoothScroll({
+      scrollTarget: '#portfolio',
+      speed: 1000
+    })
+  })
+
+  $('.skillsLink').on('click', function () {
+    $.smoothScroll({
+      scrollTarget: '#skills',
+      speed: 1000
+    })
+  })
+
+  $('.contactLink').on('click', function () {
+    $.smoothScroll({
+      scrollTarget: '#contact',
+      speed: 1000
+    })
+  })
 
   $('.navTop').on('click', function() {
     $.smoothScroll({
